92c91562efafb5cd1c271deb0284fe4a1f490154
Maarten Bakker <maartenbakker@gmail.com>
2020-11-18 20:45:09 +0100

    wrapping up

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -6,6 +6,10 @@ import {
   SimpleChanges,
   DoCheck,
   OnDestroy,
+  ViewChild,
+  ElementRef,
+  AfterViewInit,
+  ContentChild,
 } from "@angular/core";
 
 @Component({
@@ -13,9 +17,13 @@ import {
   templateUrl: "./server-element.component.html",
   styleUrls: ["./server-element.component.css"],
 })
-export class ServerElementComponent implements OnInit, OnChanges, DoCheck {
+export class ServerElementComponent
+  implements OnInit, OnChanges, DoCheck, OnDestroy, AfterViewInit {
   @Input("srvElement") element: Element;
   @Input("name") name: string;
+  @ViewChild("header", { static: true }) header: ElementRef;
+  @ContentChild("contentParagraph", { static: true })
+  contentParagraph: ElementRef;
 
   constructor() {
     console.log("Constructor called!");
@@ -28,10 +36,22 @@ export class ServerElementComponent implements OnInit, OnChanges, DoCheck {
 
   ngOnInit(): void {
     console.log("ngOnInit called!");
+    console.log(
+      "Text content of paragraph: " +
+        this.contentParagraph.nativeElement.textContent
+    );
   }
 
   ngDoCheck(): void {
     console.log("ngDoCheck called!");
+  }
+
+  ngAfterViewInit(): void {
+    console.log("text: " + this.header.nativeElement.textContent);
+    console.log(
+      "Text content of paragraph: " +
+        this.contentParagraph.nativeElement.textContent
+    );
   }
 
   ngOnDestroy(): void {
